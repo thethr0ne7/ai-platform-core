739c047a73c08c7645ccb4bf5fbaa00afe627383
thethr0ne7 <124492332+thethr0ne7@users.noreply.github.com>
2026-07-22 17:55:05 +0300

    feat(web): add intelligence graph route

---
+++ apps/web/app/control-center/graph/page.tsx
@@ -0,0 +1,5 @@
+import { IntelligenceDashboard } from '@/components/control-center/IntelligenceDashboard'
+
+export default function IntelligenceGraphPage() {
+  return <IntelligenceDashboard view="graph" />
+}
